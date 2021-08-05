8993f279de4a03420b3e1a864c877a7ea6a6b761
kripaz777 <aceraayush@gmail.com>
2021-08-05 07:55:02 +0545

    templates

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,8 @@ app_name = 'home'
 urlpatterns = [
     path('', home, name='home'),
     path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('portfolio', portfolio, name='portfolio'),
+    path('price', price, name='price'),
+    path('services', services, name='services'),
 ]
